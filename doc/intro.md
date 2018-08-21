746e18e926e6645fc710128f3d3ef48635632562
Yasuchika Onuki <yasuchikaonuki@COAMAC14100265.local>
2018-08-21 15:25:47 +0900

    initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to tut08
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/tut08/core_test.clj
@@ -0,0 +1,7 @@
+(ns tut08.core-test
+  (:require [clojure.test :refer :all]
+            [tut08.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
